dcd3ebbd2fbd1d0fd1c6ec967fe06f87f4f59665
Md Sakib Hossain <40665494+shfsakib@users.noreply.github.com>
2022-07-29 18:51:05 +0600

    website build done

--- portfolio/src/components/contact/contact.jsx
+++ portfolio/src/components/contact/contact.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md';
 import { RiFacebookCircleLine } from 'react-icons/ri';
 import { BsWhatsapp } from 'react-icons/bs';
+import emailjs from 'emailjs-com';
+
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_fa0iudj', 'template_pyxrvcj', form.current, 'B2Df2EAtu9xohCD0i')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -29,7 +43,7 @@ const Contact = () => {
                         <h5><a href="https://www.facebook.com/shfsakib" target={'_blank'}>Send a message</a></h5>
                     </article>
                 </div>
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Your Full Name' required autoComplete='off' />
                     <input type="email" name='email' placeholder='Your Email' required autoComplete='off' />
                     <textarea name="message" rows="7" placeholder='Your Message' required autoComplete='off'></textarea>
--- portfolio/src/components/footer/footer.jsx
+++ portfolio/src/components/footer/footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import './footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaWhatsapp } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { FiTwitter } from 'react-icons/fi'
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <a href="#" className='footer__logo'>Mohammad Sakib Hossain</a>
+            <ul className="permalinks">
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonial">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+            <div className="footer__socials">
+                <a href="https://www.facebook.com/shfsakib"><FaFacebookF /></a>
+                <a href="https://wa.me/8801685685506"><FaWhatsapp /></a>
+                <a href="https://www.instagram.com/shfsakib/"><FiInstagram /></a>
+                <a href="https://twitter.com/shf_sakib"><FiTwitter /></a>
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; Mohammad Sakib Hossain. All rights reserver</small>
+            </div>
+        </footer>
     )
 }
 
