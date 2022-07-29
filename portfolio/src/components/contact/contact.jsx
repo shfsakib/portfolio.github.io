2312aabd63bf964b6413321ac29a4b8d4474aa67
Md Sakib Hossain <40665494+shfsakib@users.noreply.github.com>
2022-07-29 17:44:21 +0600

    till form design

--- portfolio/src/components/contact/contact.jsx
+++ portfolio/src/components/contact/contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './contact.css'
-
+import { MdOutlineEmail } from 'react-icons/md';
+import { RiFacebookCircleLine } from 'react-icons/ri';
+import { BsWhatsapp } from 'react-icons/bs';
 const Contact = () => {
     return (
-        <section id='contact'>Contact</section>
+        <section id='contact'>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+            <div className="container contact__container">
+                <div className="contact__options">
+                    <article className="contact__option">
+                        <MdOutlineEmail className='contact__option-icon' />
+                        <h4>Email</h4>
+                        shfsakib@gmail.com
+                        <h5><a href="mailto:shfsakib@gmail.com" target={'_blank'}>Send a message</a></h5>
+                    </article>
+                    <article className="contact__option">
+                        <RiFacebookCircleLine className='contact__option-icon' />
+                        <h4>Facebook</h4>
+                        Mohammad Sakib Hossain
+                        <h5><a href="https://www.facebook.com/shfsakib" target={'_blank'}>View Profile</a></h5>
+                    </article>
+                    <article className="contact__option">
+                        <BsWhatsapp className='contact__option-icon' />
+                        <h4>Whatsapp</h4>
+                        +8801685685506
+                        <h5><a href="https://www.facebook.com/shfsakib" target={'_blank'}>Send a message</a></h5>
+                    </article>
+                </div>
+                <form action="">
+                    <input type="text" name='name' placeholder='Your Full Name' required autoComplete='off' />
+                    <input type="email" name='email' placeholder='Your Email' required autoComplete='off' />
+                    <textarea name="message" rows="7" placeholder='Your Message' required autoComplete='off'></textarea>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
--- portfolio/src/components/header/HeaderSocials.jsx
+++ portfolio/src/components/header/HeaderSocials.jsx
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-
 const HeaderSocials = () => {
     return (
         <Fragment>
@@ -7,8 +6,7 @@ const HeaderSocials = () => {
                 <a href="https://www.linkedin.com/in/shfsakib/" target='_blank'><img src="./dist/img/linkedin.png" alt="linkedin" /></a>
                 <a href="https://github.com/shfsakib/" target='_blank'><img src="./dist/img/github.png" className='githubImg' alt="github" /></a>
                 <a href="https://www.fiverr.com/shfsakib?up_rollout=true" target='_blank'><img src="./dist/img/fiverr.png" alt="fiverr" /></a>
-                <a href="https://wa.me/8801685685506" target='_blank'><img src="./dist/img/whatsapp.png" alt="whatsapp" /></a>
-                <a href="https://www.facebook.com/shfsakib" target='_blank'><img src="./dist/img/facebook.png" alt="facebook" /></a>
+
             </div>
         </Fragment>
     )
